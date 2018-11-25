50a49d9e242c74a4138a7df98d3e3586296c2547
Jesse Cai <jcjessecai@gmail.com>
2018-11-24 21:24:49 -0800

    more work on lifted inference

--- parser.py
+++ parser.py
@@ -1,58 +1,96 @@
 from pyparsing import *
+import logging
+
+logging.basicConfig(level=logging.INFO)
+_LOGGER = logging.getLogger(__name__)
 
 test_query = "R(x1),X(s1,y1) || S(x2, y2), T(x2)"
 
-varname = Word(alphas, alphanums)
-tablename = Word(alphas.upper(), exact=1)
-table = tablename.setResultsName('table', listAllMatches=True) + Suppress('(') + \
-        delimitedList(varname, delim=',').setResultsName('vars', listAllMatches=True) + Suppress(')')
-conj = delimitedList(table, delim=',').setResultsName('conj', listAllMatches=True)
-query = delimitedList(conj, delim='||').setResultsName('query', listAllMatches=True)
+varname = Word(alphas, alphanums).setResultsName('vars', listAllMatches=True)
+tablename = Word(alphas.upper(), exact=1).setResultsName('table', listAllMatches=True)
+table = (tablename + '(' + (varname + ZeroOrMore(',' + varname)) + ')').setResultsName('clause', listAllMatches=True)
+conj = (table+ ZeroOrMore(',' + table)).setResultsName('conj', listAllMatches=True)
+query = (conj + ZeroOrMore('||' + conj)).setResultsName('query', listAllMatches=True)
 
 parsed_query = query.parseString(test_query, parseAll=True)
 print(parsed_query.dump())
-print("Doing lifted inference")
+
+print("\n Doing lifted inference... \n")
 
 def lift(Q, P):
-    print("working on query : {}".format(Q))
+    _LOGGER.info("working on query : {}".format(''.join(Q)))
+
+    # recursion base case
     if len(Q.table) == 1:
-        print("Lookup prob for {}".format(''.join(Q)))
-        #P.lookup Q in the future
-        return 0.5
+        grounded = P.lookup(Q.table.pop(), Q.vars)
+        if grounded:
+            _LOGGER.info("Lookup prob for {} = {}".format(''.join(Q), grounded))
+            #P.lookup Q in the future
+            return grounded
 
     if len(Q.conj) > 1:
         # independent if share no tables
+        # finds decomposable disjunction
         q1, q2 = None, None
         for conj in Q.conj:
             if q1 is None:
-                q1 = conj
-            if not set(q1.table) & set(conj.table):
-                q2 = conj
-        
-        print("q1: {}".format(q1))
-        print("q2: {}".format(q2))
-        # existst decomposable disjunction
+                q1 = query.parseString(''.join(conj))
+            if not (set(q1.table) & set(conj.table)):
+                q2 = query.parseString(''.join(conj))
+                break
+
         if q1 and q2:
+            _LOGGER.info("found decomposable disjunction q1: {} and q2: {}".format(''.join(q1), ''.join(q2)))
             return 1-((1-lift(q1, P)) * (1-lift(q2, P)))
-
         else:
-            print("Use inclusion-exclusion")
+            _LOGGER.info("Use inclusion-exclusion")
 
-    #decomposable conjunction
-    if (Q.getName() == 'conj'):
+    if len(Q.conj) == 1:
+        #try to find a decomposable conjunction (indpendent if doesn't share tables or vars)
         q1, q2 = None, None
-        for t, v in zip(Q.table, Q.vars):
+        for clause in Q.clause:
             if q1 is None:
-                q1 = (t, v)
-            if not ()
+                q1 = query.parseString(''.join(clause))
+            if not (set(q1.vars) & set(clause.vars) or set(q1.table) & set(clause.table)):
+                q2 = query.parseString(''.join(clause))
+                break
+
+        # decomposable conjunction
+        if q2 and q2:
+            _LOGGER.info("found decomposable conjunction q1: {} and q2: {}".format(''.join(q1), ''.join(q2)))
+            return lift(q1, P) * lift(q2, P)
+
+        # try to find a seperator variable only 
+        seperator_var_set = set.intersection(*[set(clause.vars) for clause in Q.clause])
+        if seperator_var_set:
+            seperator_var = seperator_var_set.pop()
+            _LOGGER.info("found decomposable quantifier with sep var: {}".format(seperator_var))
+
+            print(P.ground(Q.table.pop(), Q.vars))
+            return
+
 
     # decomposable universal quantifier
-    
-
-    return 0
+    raise ValueError("Query {} is non-heirarchical!".format(Q))
 
 
+class PDB():
 
+    R = {('1',): 0.4}
+    X = {('1','1'): 0.4}
+    S = {('1','1'): 0.4}
+    T = {('1',): 0.4}
 
-print(lift(parsed_query, 1))
+    def lookup(self, table, var):
+        try:
+            return getattr(self, table)[tuple(var)]
+        except Exception:
+            return False
+
+    def ground(self, table, var):
+        return [key for key in getattr(self, table)]
+            
+
+pdb = PDB()
+_LOGGER.info(lift(parsed_query, pdb))
 
