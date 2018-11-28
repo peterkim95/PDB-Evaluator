aac1ee148bd1665f5511f86966e46ab6ca2f85f9
Jesse Cai <jcjessecai@gmail.com>
2018-11-27 19:10:44 -0800

    did some wor

--- parser.py
+++ parser.py
@@ -10,9 +10,9 @@ from pprint import pprint
 logging.basicConfig(level=logging.INFO)
 _LOGGER = logging.getLogger(__name__)
 
-test_query = "P(x1),R(x1,y1) || R(x3, z2), Q(x1)"
+# test_query = "P(x1) || Q(x2)"
 # test_query = "R(x1, y1), Q(x1)"
-# test_query = "R(x1, y1), P(x1), Q(x2), R(x2, y2)"
+test_query = "R(x1, y1), P(x1), Q(x2), R(x2, y2) || P(x1)"
 
 varname = Word(alphas, alphanums).setResultsName('vars', listAllMatches=True)
 tablename = Word(alphas.upper(), exact=1).setResultsName('table', listAllMatches=True)
@@ -34,11 +34,28 @@ def lift(query_string, pdb, subsitutions):
     def set_vars(x):
         return set(x.vars)
 
+    # for finding independence (connected components)
+    def union_find(lis):
+        lis = map(set, lis)
+        unions = []
+        for item in lis:
+            temp = []
+            for s in unions:
+                if not s.isdisjoint(item):
+                    item = s.union(item)
+                else:
+                    temp.append(s)
+            temp.append(item)
+            unions = temp
+        return unions
+
     indent = '.....' * (len(traceback.extract_stack()) - 2)
+    if len(traceback.extract_stack()) > 50:
+        raise ValueError("Recursion")
 
     Q = query.parseString(query_string)
     _LOGGER.info("{} LIFT: working on query: {} with subsitutions: {}".format(indent, pretty(Q), subsitutions))
-
+   
     # base case of recursion, 1 table and vars all instantiated 
     if len(Q.table) == 1 and subsitutions.keys() == set(Q.vars):
         table = Q.table.pop()
@@ -46,52 +63,62 @@ def lift(query_string, pdb, subsitutions):
         _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
         return prob
 
-    if len(Q.conj) > 1:
-        #indepndent if q1 and q2 have different vars not in subsitutions
-        q1, q2 = [], []
-        for a in Q.conj:
-            if not q1 or set(a.vars) & set.union(*map(set_vars,q1)) - subsitutions.keys():
-                q1.append(a)
-            else:
-                q2.append(a)
-        q1 =  '||'.join(map(''.join, q1))
-        q2 =  '||'.join(map(''.join, q2))
+    if len(Q.conj) == 1:
+        def dependent(combination):
+            q1, q2 = combination
+            return set(q1.table) & set(q2.table) or set(q1.vars) & set(q2.vars) - subsitutions.keys()
 
-        if q1 and q2:
-            _LOGGER.info("{} DECOMPOSABLE DISJUNCTION: q1: {} and q2: {}".format(indent, pretty(q1), pretty(q2)))
-            prob = 1-((1-lift(q1, pdb, subsitutions))\
-                     *(1-lift(q2, pdb, subsitutions)))
+        def make_clause(x):
+            return map(''.join, x)
+
+        test = map(make_clause, filter(dependent, itertools.combinations(Q.clause, 2)))
+        dependent_components = union_find(test)
+        if not dependent_components:
+            dependent_components = [{''.join(clause)} for clause in Q.clause]
+
+        print(dependent_components)
+
+        # all independent
+        if len(dependent_components) > 1 :
+            _LOGGER.info("{} DECOMPOSABLE DISJUNCTION:  {} ".format(indent, dependent_components))
+            prob = 1-reduce(operator.mul, map(lambda x: 1-lift(''.join(x), pdb, subsitutions), dependent_components))
             _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
             return prob
 
         else:
-            #TODO figure out inclusion exclusion
-            # at this point we know its one big conj with all unions
+            # at this point we know its one big disjunction
             _LOGGER.info("{} INCLUSION EXCLUSION: query {}".format(indent, pretty(Q)))
-            test = [''.join(c) for c in Q.conj]
+            test = [''.join(c) for c in Q.clause]
+            print(test)
             a = [math.pow(-1, i) * sum(map(lambda x: lift(','.join(x), pdb, subsitutions), itertools.combinations(test, i+1))) for i in range(len(Q.conj))]
-            print(a)
+            raise ValueError()
             prob = sum(a)
             _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
             return prob
-                
 
     # now we are only dealing with conjunctions 
-    if len(Q.conj) == 1:
-        #indepndent if q1 and q2 have different tables and vars not in subsitutions
-        q1, q2 = [], []
-        for a in Q.clause:
-            if not q1 or set(a.vars) & set.union(*map(set_vars,q1)) - subsitutions.keys():
-                q1.append(a)
-            else:
-                q2.append(a)
-        q1 =  ','.join(map(''.join, q1))
-        q2 =  ','.join(map(''.join, q2))
+    if len(Q.conj) > 1:
 
-        # decomposable conjunction
-        if q1 and q2:
-            _LOGGER.info("{} DECOMPOSABLE CONJUNCTION: q1: {} and q2: {}".format(indent, pretty(q1), pretty(q2)))
-            prob = lift(q1, pdb, subsitutions) * lift(q2, pdb, subsitutions)
+        #indepndent if q1 and q2 have different tables and vars not in subsitutions
+        def dependent(combination):
+            q1, q2 = combination
+            return set(q1.table) & set(q2.table) or set(q1.vars) & set(q2.vars) - subsitutions.keys()
+
+        def make_clause(x):
+            return map(''.join, x)
+
+        #shitty way of doing this but whatever
+        test = map(make_clause, filter(dependent, itertools.combinations(Q.clause, 2)))
+        dependent_components = union_find(test)
+        if not dependent_components:
+            dependent_components = [{''.join(clause)} for clause in Q.clause]
+
+        print(dependent_components)
+
+        # all independent
+        if len(dependent_components) > 1 :
+            _LOGGER.info("{} DECOMPOSABLE CONJUNCTION: {} ".format(indent, dependent_components))
+            prob = reduce(operator.mul, map(lambda x: lift(','.join(x), pdb, subsitutions), dependent_components))
             _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
             return prob
 
@@ -117,7 +144,7 @@ def lift(query_string, pdb, subsitutions):
             return prob
 
     # decomposable universal quantifier
-    raise ValueError("Query {} is non-heirarchical!".format(''.join(Q)))
+    raise ValueError("Query {} is non-heirarchical!".format(pretty(Q)))
 
 class PDB():
 
@@ -143,8 +170,13 @@ class PDB():
 
     def lookup(self, table, var):
         try:
-            return getattr(self, table)[tuple(var)]
+            key = tuple(var)
+            # print("Doing lookup for {}".format(key))
+            a = getattr(self, table)[key]
+            # print("found {}".format(a))
+            return a
         except Exception as e:
+            # _LOGGER.exception(e)
             return 0
 
     def ground(self, table, varindex):
@@ -152,6 +184,7 @@ class PDB():
             
 
 pdb = PDB()
+parsed_query = query.parseString(test_query)
 # print(parsed_query.dump())
 _LOGGER.info(lift(test_query, pdb, dict()))
 
