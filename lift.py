a077f5c9d775acc3adb962b1572c5d95098495bc
Peter Kim <peterkim95@ucla.edu>
2018-12-04 16:42:07 -0800

    sql integration and cleanup

--- lift.py
+++ lift.py
@@ -7,237 +7,202 @@ import traceback
 import math
 from pprint import pprint
 
+from table import SQLDatabase
+
 logging.basicConfig(level=logging.INFO)
 _LOGGER = logging.getLogger(__name__)
 
-# test_query = "Q(x1)"
-# test_query = "R(x1, y1) || Q(x1)"
-test_query = "R(x1, y1)|| P(x1)|| Q(x2)|| R(x2, y2)"
+class Lifter:
 
-# E abcd (a and b and c and d)
-# !A abcd !(a and b and c and d)
-#         !a or !b or !c or !d
+    # test_query = "Q(x1)"
+    # test_query = "R(x1, y1) || Q(x1)"
+    # test_query = "R(x1, y1)|| P(x1)|| Q(x2)|| R(x2, y2)"
 
-# E abcd (a and  b)  or (c and d)
+    # E abcd (a and b and c and d)
+    # !A abcd !(a and b and c and d)
+    #         !a or !b or !c or !d
 
-
-# !A abcd !((a and b) or (c and d))
-#         !((a and b) or (c and d))
-#         !(a and  b) and !(c and d)
-#
-# !A abcd (!a or !b) and  (!c or !d)
-
-varname = Word(alphas, alphanums).setResultsName('vars', listAllMatches=True)
-tablename = Word(alphas.upper(), exact=1).setResultsName('table', listAllMatches=True)
-clause = (tablename + '(' + (varname + ZeroOrMore(',' + varname)) + ')').setResultsName('clause', listAllMatches=True)
-conj = (clause + ZeroOrMore(',' + clause)).setResultsName('conj', listAllMatches=True)
-query = (conj + ZeroOrMore('||' + conj)).setResultsName('query', listAllMatches=True)
-
-#number of ands is #of clauses - # of conj
-
-print("\n Doing lifted inference... \n")
+    # E abcd (a and  b)  or (c and d)
 
 
-def invert_f(f):
-    def helper(x):
-        return 1-f(x)
-    return helper
+    # !A abcd !((a and b) or (c and d))
+    #         !((a and b) or (c and d))
+    #         !(a and  b) and !(c and d)
+    #
+    # !A abcd (!a or !b) and  (!c or !d)
 
-#first convert the query  to universal and then run the universal alg
+    def __init__(self, table_files):
+        self.pdb = SQLDatabase(table_files=table_files)
 
+        varname = Word(alphas, alphanums).setResultsName('vars', listAllMatches=True)
+        tablename = Word(alphas.upper(), exact=1).setResultsName('table', listAllMatches=True)
+        clause = (tablename + '(' + (varname + ZeroOrMore(',' + varname)) + ')').setResultsName('clause', listAllMatches=True)
+        conj = (clause + ZeroOrMore(',' + clause)).setResultsName('conj', listAllMatches=True)
+        self.query = (conj + ZeroOrMore('||' + conj)).setResultsName('query', listAllMatches=True)
 
-def lift(query_string, pdb, subsitutions, invertLiterals=True):
-    #helper
-    def union_find(lis):
-        lis = map(set, lis)
-        unions = []
-        for item in lis:
-            temp = []
-            for s in unions:
-                if not s.isdisjoint(item):
-                    item = s.union(item)
-                else:
-                    temp.append(s)
-            temp.append(item)
-            unions = temp
-        return unions
+    def _lift_helper(self, query_string, subsitutions, invertLiterals=True):
+        #helper
+        def union_find(lis):
+            lis = map(set, lis)
+            unions = []
+            for item in lis:
+                temp = []
+                for s in unions:
+                    if not s.isdisjoint(item):
+                        item = s.union(item)
+                    else:
+                        temp.append(s)
+                temp.append(item)
+                unions = temp
+            return unions
 
-    def dependent(q1, q2):
-        return share_vars(q1, q2) or share_tables(q1, q2)
+        def dependent(q1, q2):
+            return share_vars(q1, q2) or share_tables(q1, q2)
 
-    def share_vars(q1, q2):
-        return set(q1.vars) & set(q2.vars) - subsitutions.keys()
+        def share_vars(q1, q2):
+            return set(q1.vars) & set(q2.vars) - subsitutions.keys()
 
-    def share_tables(q1, q2):
-        return set(q1.table) & set(q2.table)
+        def share_tables(q1, q2):
+            return set(q1.table) & set(q2.table)
 
-    def make_clause(x):
-        return map(''.join, x)
+        def make_clause(x):
+            return map(''.join, x)
 
-    def pretty(Q):
-        formatted = ''.join(Q)
-        for var in subsitutions:
-            formatted = formatted.replace(var, subsitutions[var])
-        return formatted
+        def pretty(Q):
+            formatted = ''.join(Q)
+            for var in subsitutions:
+                formatted = formatted.replace(var, subsitutions[var])
+            return formatted
 
-    indent = '  .' * (len(traceback.extract_stack()) - 2)
-    Q = query.parseString(query_string)
-    _LOGGER.info("{} LIFT: working on query: {} with subsitutions: {}".format(indent, pretty(Q), subsitutions))
+        indent = '  .' * (len(traceback.extract_stack()) - 2)
+        Q = self.query.parseString(query_string)
+        _LOGGER.info("{} LIFT: working on query: {} with subsitutions: {}".format(indent, pretty(Q), subsitutions))
 
-    # base case of recursion, 1 table and vars all instantiated
-    if len(Q.table) == 1 and subsitutions.keys() == set(Q.vars):
-        table = Q.table.pop()
-        prob = pdb.lookup(table, (subsitutions[v] for v in Q.vars))
-        _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
-        if invertLiterals:
-            return 1 - prob
-        return prob
-
-    if len(Q.conj) > 1:
-
-        test = list(map(make_clause, filter(lambda x: dependent(*x), itertools.combinations(Q.conj, 2))))
-        dependent_components = union_find(test)
-        if not dependent_components:
-            dependent_components = [{''.join(conj)} for conj in Q.conj]
-
-        # all independent
-        if len(dependent_components) > 1 :
-            _LOGGER.info("{} DECOMPOSABLE DISJUNCTION: {} ".format(indent, dependent_components))
-            prob = 1-reduce(operator.mul, map(lambda x: 1-lift(','.join(x), pdb, subsitutions), dependent_components))
+        # base case of recursion, 1 table and vars all instantiated
+        if len(Q.table) == 1 and subsitutions.keys() == set(Q.vars):
+            table = Q.table.pop()
+            prob = self.pdb.lookup(table, (subsitutions[v] for v in Q.vars))
             _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
+            if invertLiterals:
+                return 1 - prob
             return prob
 
-        else:
-            _LOGGER.info("{} INCLUSION EXCLUSION: query {}".format(indent, pretty(Q)))
+        if len(Q.conj) > 1:
 
-            test = [''.join(c) for c in Q.clause]
-            #partition test into (varsets)
-            varstest = map(make_clause, list(filter(lambda x: share_vars(*x), itertools.combinations(Q.conj, 2))))
-            varstest = union_find(varstest)
-            if not varstest:
-                varstest = [{''.join(conj)} for conj in Q.conj]
+            test = list(map(make_clause, filter(lambda x: dependent(*x), itertools.combinations(Q.conj, 2))))
+            dependent_components = union_find(test)
+            if not dependent_components:
+                dependent_components = [{''.join(conj)} for conj in Q.conj]
 
-            varstest = list(map('||'.join, varstest))
-            print(varstest)
-
-            #break on varset (possible to run inclusion exclusion)
-            if len(varstest) > 1:
-
-                def merge(combination):
-                    #base case
-                    if len(combination) == 1:
-                        return ''.join(combination[0])
-
-                    #see if there is clause to be factored out
-                    subqueries = list(map(query.parseString, combination))
-                    test = set.intersection(*[set(a.table) for a in subqueries])
-                    print(test)
-
-                    unifier = None
-                    vars_to_convert = []
-
-                    for q in subqueries:
-                        for clause in q.clause:
-                            if clause.table.pop() in test:
-                                if not unifier:
-                                    unifier = clause
-                                else:
-                                    vars_to_convert.append(clause.vars)
-
-                    # print("unifier:", unifier)
-                    # print("vars:", vars_to_convert)
-
-                    factored = [','.join(map(''.join, filter(lambda x: x.table.pop() not in test, subq.clause))) for subq in subqueries]
-                    print(factored)
-
-                    prev = ','.join(factored)
-                    for varthing in vars_to_convert:
-                        for var, unify in zip(varthing, unifier.vars):
-                            prev = prev.replace(var, unify)
-
-                    return '||'.join([''.join(unifier), prev])
-
-
-                prob = sum([math.pow(-1, i) * sum(map(lambda x: lift(merge(x), pdb, subsitutions), itertools.combinations(varstest, i+1))) for i in range(len(varstest))])
+            # all independent
+            if len(dependent_components) > 1 :
+                _LOGGER.info("{} DECOMPOSABLE DISJUNCTION: {} ".format(indent, dependent_components))
+                prob = 1-reduce(operator.mul, map(lambda x: 1-self._lift_helper(','.join(x), subsitutions), dependent_components))
                 _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
                 return prob
 
-            _LOGGER.info("INCLUSION-EXCLUSION failed")
+            else:
+                _LOGGER.info("{} INCLUSION EXCLUSION: query {}".format(indent, pretty(Q)))
+
+                test = [''.join(c) for c in Q.clause]
+                #partition test into (varsets)
+                varstest = map(make_clause, list(filter(lambda x: share_vars(*x), itertools.combinations(Q.conj, 2))))
+                varstest = union_find(varstest)
+                if not varstest:
+                    varstest = [{''.join(conj)} for conj in Q.conj]
+
+                varstest = list(map('||'.join, varstest))
+                print(varstest)
+
+                #break on varset (possible to run inclusion exclusion)
+                if len(varstest) > 1:
+
+                    def merge(combination):
+                        #base case
+                        if len(combination) == 1:
+                            return ''.join(combination[0])
+
+                        #see if there is clause to be factored out
+                        subqueries = list(map(self.query.parseString, combination))
+                        test = set.intersection(*[set(a.table) for a in subqueries])
+                        print(test)
+
+                        unifier = None
+                        vars_to_convert = []
+
+                        for q in subqueries:
+                            for clause in q.clause:
+                                if clause.table.pop() in test:
+                                    if not unifier:
+                                        unifier = clause
+                                    else:
+                                        vars_to_convert.append(clause.vars)
+
+                        # print("unifier:", unifier)
+                        # print("vars:", vars_to_convert)
+
+                        factored = [','.join(map(''.join, filter(lambda x: x.table.pop() not in test, subq.clause))) for subq in subqueries]
+                        print(factored)
+
+                        prev = ','.join(factored)
+                        for varthing in vars_to_convert:
+                            for var, unify in zip(varthing, unifier.vars):
+                                prev = prev.replace(var, unify)
+
+                        return '||'.join([''.join(unifier), prev])
 
 
-    #check for decomposabvle disjunction (just one conj)
-    test = map(make_clause, filter(lambda x : dependent(*x), itertools.combinations(Q.clause, 2)))
-    dependent_components = union_find(test)
-    if not dependent_components:
-        dependent_components = [{''.join(clause)} for clause in Q.clause]
+                    prob = sum([math.pow(-1, i) * sum(map(lambda x: self._lift_helper(merge(x), subsitutions), itertools.combinations(varstest, i+1))) for i in range(len(varstest))])
+                    _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
+                    return prob
 
-    # all independent
-    if len(dependent_components) > 1 :
-        _LOGGER.info("{} DECOMPOSABLE DISJUNCTION:  {} ".format(indent, dependent_components))
-        prob = reduce(operator.mul, map(lambda x: lift(''.join(x), pdb, subsitutions), dependent_components))
-        _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
-        return prob
+                _LOGGER.info("INCLUSION-EXCLUSION failed")
 
-    # try to find a seperator variable only
-    seperator_var_set = set.intersection(*[set(clause.vars) for clause in Q.clause]) - subsitutions.keys()
-    if seperator_var_set:
-        seperator_var = seperator_var_set.pop()
-        _LOGGER.info("{} DECOMPOSABLE QUANTIFIER: query {} has separator var: {}".format(indent, pretty(Q), seperator_var))
 
-        def generate_grounding(seperator_var, grounding):
-            new = subsitutions.copy()
-            new[seperator_var] = grounding
-            return new
+        #check for decomposabvle disjunction (just one conj)
+        test = map(make_clause, filter(lambda x : dependent(*x), itertools.combinations(Q.clause, 2)))
+        dependent_components = union_find(test)
+        if not dependent_components:
+            dependent_components = [{''.join(clause)} for clause in Q.clause]
 
-        def get_possible_vals(clause):
-            return pdb.ground(clause.table.pop(), list(clause.vars).index(seperator_var))
+        # all independent
+        if len(dependent_components) > 1 :
+            _LOGGER.info("{} DECOMPOSABLE DISJUNCTION:  {} ".format(indent, dependent_components))
+            prob = reduce(operator.mul, map(lambda x: self._lift_helper(''.join(x), subsitutions), dependent_components))
+            _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
+            return prob
 
-        # note that a grounding is only non-zero if it's present in all of the tables, so we can use intersection here
-        new_subsitutions = [generate_grounding(seperator_var, grounding) for grounding in set.intersection(*map(get_possible_vals, Q.clause))]
-        _LOGGER.info("{} creating {} new subsitutions: {}".format(indent, len(new_subsitutions), new_subsitutions))
-        prob =  reduce(operator.mul, map(lambda x: lift(query_string, pdb, x), new_subsitutions))
-        _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
-        return prob
+        # try to find a seperator variable only
+        seperator_var_set = set.intersection(*[set(clause.vars) for clause in Q.clause]) - subsitutions.keys()
+        if seperator_var_set:
+            seperator_var = seperator_var_set.pop()
+            _LOGGER.info("{} DECOMPOSABLE QUANTIFIER: query {} has separator var: {}".format(indent, pretty(Q), seperator_var))
 
-    raise ValueError("Query: {} is non-heirarchical!".format(pretty(Q)))
+            def generate_grounding(seperator_var, grounding):
+                new = subsitutions.copy()
+                new[seperator_var] = grounding
+                return new
 
-class PDB():
+            def get_possible_vals(clause):
+                return self.pdb.ground(clause.table.pop(), list(clause.vars).index(seperator_var))
 
-    P = {
-            ('0',): 0.7,
-            ('1',): 0.8,
-            ('2',): 0.6,
-        }
+            # note that a grounding is only non-zero if it's present in all of the tables, so we can use intersection here
+            new_subsitutions = [generate_grounding(seperator_var, grounding) for grounding in set.intersection(*map(get_possible_vals, Q.clause))]
+            _LOGGER.info("{} creating {} new subsitutions: {}".format(indent, len(new_subsitutions), new_subsitutions))
+            prob =  reduce(operator.mul, map(lambda x: self._lift_helper(query_string, x), new_subsitutions))
+            _LOGGER.info("{} RESULT: Prob of query: {} = {}".format(indent, pretty(Q), prob))
+            return prob
 
-    Q = {
-            ('0',): 0.7,
-            ('1',): 0.3,
-            ('2',): 0.5,
-        }
+        raise ValueError("Query: {} is non-heirarchical!".format(pretty(Q)))
 
-    R = {
-            ('0','0'): 0.8,
-            ('0','1'): 0.4,
-            ('0','2'): 0.5,
-            ('1','2'): 0.6,
-            ('2','2'): 0.9,
-        }
 
-    def lookup(self, table, var):
-        try:
-            key = tuple(var)
-            # print("Doing lookup for {}".format(key))
-            a = getattr(self, table)[key]
-            # print("found {}".format(a))
-            return a
-        except Exception as e:
-            # _LOGGER.exception(e)
-            return 0
+    def lift(self, query):
+        return 1 - self._lift_helper(query, dict(), invertLiterals=True)
 
-    def ground(self, table, varindex):
-        return set([key[varindex] for key in getattr(self, table)])
 
-pdb = PDB()
-parsed_query = query.parseString(test_query)
-# print(parsed_query.ump())
-_LOGGER.info(1 - lift(test_query, pdb, dict(), invertLiterals=True))
+def main():
+    # test for test_query1
+    print(Lifter(['data/table_files/T1.txt', 'data/table_files/T2.txt', 'data/table_files/T3.txt']).lift('R(x1, y1) || Q(x1)'))
+
+if __name__ == '__main__':
+    main()
--- pdb.py
+++ pdb.py
@@ -1,15 +1,20 @@
 import argparse
-import logging
-
-import lift
-import table
+from lift import Lifter
 
 def main():
     parser = argparse.ArgumentParser(description='perform pdb inference')
     parser.add_argument('-q', '--query', help='path to query file')
-    parser.add_argument('-t', '--table', help='path to table file')
+    parser.add_argument('-t', '--table', action='append', nargs='*', help='path to table file')
     parser.add_argument('-m', '--mcmc', help='use MCMC', action='store_true', default=False)
     args = parser.parse_args()
+    args.table = [item for sublist in args.table for item in sublist] # flatten 2d list
+
+    l = Lifter(args.table)
+    # perform inference for all queries
+    with open(args.query) as f:
+        for query in f:
+            q = query.rstrip('\n')
+            print('*** Pr({}) = {} ***'.format(q, l.lift(q)))
 
 if __name__ == '__main__':
     main()
